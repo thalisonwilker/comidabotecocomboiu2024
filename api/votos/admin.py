f5a261a47cbb884299addd35cf8be79e6039b273
Thalyson <thalison.wilker@gmail.com>
2024-04-05 12:27:40 -0300

    atualiza relatório

--- api/votos/admin.py
+++ api/votos/admin.py
@@ -9,15 +9,15 @@ from django.db.models import Sum
 
 
 def export_to_excel(modeladmin, request, queryset):
-    # Cria um workbook e uma worksheet
     wb = openpyxl.Workbook()
     ws = wb.active
     ws.title = "Todos os votos".upper()
     ws2 = wb.create_sheet(title="Contagem dos votos".upper())
 
     ws.append(["Bar", "CPF", "Apresentação do Prato", "Aroma do Prato",
-              "Sabor do Prato", "Tempo de espera", "Bebida", "Higiene do local", "Hambiente"])
-    ws2.append(["Bar", "Apresentação do Prato", "Aroma do Prato", "Sabor do Prato",
+              "Sabor do Prato","Atendimento", "Tempo de espera", "Bebida", "Higiene do local", "Hambiente"])
+
+    ws2.append(["Bar", "Apresentação do Prato", "Aroma do Prato", "Sabor do Prato", "Atendimento",
                "Tempo de espera", "Bebida", "Higiene do local", "Hambiente"])
 
     data_obj = {}
@@ -31,21 +31,31 @@ def export_to_excel(modeladmin, request, queryset):
                 "total_apresentacao_prato": votos.aggregate(
                     total_apresentacao_prato=Sum("apresentacao_prato")
                 ).get("total_apresentacao_prato", 0) or 0,
+
                 "total_aroma_prato": votos.aggregate(
                     total_aroma_prato=Sum("aroma_prato")
                 ).get("total_aroma_prato", 0) or 0,
+
                 "total_sabor_prato": votos.aggregate(
                     total_sabor_prato=Sum("sabor_prato")
                 ).get("total_sabor_prato", 0) or 0,
+
+                "total_atendimento": votos.aggregate(
+                    total_atendimento=Sum("atendimento")
+                ).get("total_atendimento", 0) or 0,
+
                 "total_tempo_espera": votos.aggregate(
                     total_tempo_espera=Sum("tempo_espera")
                 ).get("total_tempo_espera", 0) or 0,
+
                 "total_bebida": votos.aggregate(
                     total_bebida=Sum("bebida")
                 ).get("total_bebida", 0) or 0,
+
                 "total_higiene_local": votos.aggregate(
                     total_higiene_local=Sum("higiene_local")
                 ).get("total_higiene_local", 0) or 0,
+
                 "total_ambiente": votos.aggregate(
                     total_ambiente=Sum("ambiente")
                 ).get("total_ambiente", 0) or 0
@@ -53,21 +63,22 @@ def export_to_excel(modeladmin, request, queryset):
 
     for obj in Votos.objects.all():
         row = [obj.bar.nome, obj.cpf, obj.apresentacao_prato, obj.aroma_prato,
-               obj.sabor_prato, obj.tempo_espera, obj.bebida, obj.higiene_local, obj.ambiente]
+               obj.sabor_prato, obj.atendimento, obj.tempo_espera, obj.bebida, obj.higiene_local, obj.ambiente]
         ws.append(row)
 
-    
     for aba2 in data_obj.keys():
         bar = aba2
         total_apresentacao_prato = data_obj[aba2]["total_apresentacao_prato"]
         total_aroma_prato = data_obj[aba2]["total_aroma_prato"]
         total_sabor_prato = data_obj[aba2]["total_sabor_prato"]
+        total_atendimento = data_obj[aba2]["total_atendimento"]
         total_tempo_espera = data_obj[aba2]["total_tempo_espera"]
         total_bebida = data_obj[aba2]["total_bebida"]
         total_higiene_local = data_obj[aba2]["total_higiene_local"]
         total_ambiente = data_obj[aba2]["total_ambiente"]
 
-        row = [aba2, total_apresentacao_prato, total_aroma_prato, total_aroma_prato, total_sabor_prato, total_tempo_espera, total_bebida, total_higiene_local, total_ambiente]
+        row = [aba2, total_apresentacao_prato, total_aroma_prato, total_aroma_prato, total_sabor_prato,
+               total_atendimento, total_tempo_espera, total_bebida, total_higiene_local, total_ambiente]
         ws2.append(row)
 
     # Salva o workbook em um objeto BytesIO em memória
